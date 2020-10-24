492008055053c704d53aca913bb644322f381dec
User <user@user.user>
2020-10-24 23:14:16 +0300

    Bug fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 import datetime
 import os
 import dotenv
-dotenv.load_dotenv()
+dotenv.load_dotenv(encoding='utf-8')
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
@@ -30,6 +30,7 @@ if len(TARGET_CHAT_NAMES_LIST) != len(SOURCE_CHAT_NAMES_LIST):
 
 SOURCE_TARGET_CHAT_MAP = {}
 
+source_chats_set = set()
 for i in range(0, len(TARGET_CHAT_NAMES_LIST)):
     TARGET_CHAT_NAME = TARGET_CHAT_NAMES_LIST[i]
     SOURCE_CHAT_NAMES = SOURCE_CHAT_NAMES_LIST[i].split(",,,")  # in case you need multiple chats - split them by ,,,
@@ -42,14 +43,22 @@ for i in range(0, len(TARGET_CHAT_NAMES_LIST)):
     source_chat_names = list(map(lambda c: c.name, source_chats))
     print("SRC CHATS: {}".format(source_chat_names))
 
-    for source_chat_id in source_chats:
-        SOURCE_TARGET_CHAT_MAP[source_chat_id] = target_chat
+    for source_chat in source_chats:
+        source_chats_set.add(source_chat)
+        if source_chat.id in SOURCE_TARGET_CHAT_MAP:
+            SOURCE_TARGET_CHAT_MAP[source_chat.id].append(target_chat)
+        else:
+            SOURCE_TARGET_CHAT_MAP[source_chat.id] = [target_chat]
 
-@client.on(events.NewMessage(chats=source_chats))
+for key in SOURCE_TARGET_CHAT_MAP:
+    print(key, '->', SOURCE_TARGET_CHAT_MAP[key])
+
+@client.on(events.NewMessage(chats=list(source_chats_set)))
 async def handler(event):
     if hasattr(event, 'message'):
         print(event)
-        await client.forward_messages(SOURCE_TARGET_CHAT_MAP[event.chat_id], event.message, event.chat_id)
+        for target_chat in SOURCE_TARGET_CHAT_MAP[event.chat_id]:
+            await client.forward_messages(target_chat, event.message, event.chat_id)
 
 
 client.run_until_disconnected()
