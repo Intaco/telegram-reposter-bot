ecb1d8c9633dc0526f979025a8921bfbb30abeb3
Ionnia <baymakanov@gmail.com>
2022-01-20 21:18:42 +0300

    Now using channel_id instead of chat_id to identify messages to resend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient, events, sync
+from telethon.tl.types import InputPeerChannel
 import schedule
 import socks
 import logging
@@ -45,20 +46,22 @@ for i in range(0, len(TARGET_CHAT_NAMES_LIST)):
 
     for source_chat in source_chats:
         source_chats_set.add(source_chat)
-        if source_chat.id in SOURCE_TARGET_CHAT_MAP:
-            SOURCE_TARGET_CHAT_MAP[source_chat.id].append(target_chat)
+        if source_chat.entity.id in SOURCE_TARGET_CHAT_MAP:
+            SOURCE_TARGET_CHAT_MAP[source_chat.entity.id].append(target_chat)
         else:
-            SOURCE_TARGET_CHAT_MAP[source_chat.id] = [target_chat]
+            SOURCE_TARGET_CHAT_MAP[source_chat.entity.id] = [target_chat]
 
 for key in SOURCE_TARGET_CHAT_MAP:
     print(key, '->', SOURCE_TARGET_CHAT_MAP[key])
 
 @client.on(events.NewMessage())
 async def handler(event):
-    if hasattr(event, 'message') and event.chat_id in SOURCE_TARGET_CHAT_MAP:
-        print(event)
-        for target_chat in SOURCE_TARGET_CHAT_MAP[event.chat_id]:
-            await client.forward_messages(target_chat, event.message, event.chat_id)
+    if isinstance(event.input_chat, InputPeerChannel):
+        source_channel_id = event.input_chat.channel_id
+        if hasattr(event, 'message') and source_channel_id in SOURCE_TARGET_CHAT_MAP:
+            print(event)
+            for target_chat in SOURCE_TARGET_CHAT_MAP[source_channel_id]:
+                await client.forward_messages(target_chat, event.message)
 
 
 client.run_until_disconnected()
